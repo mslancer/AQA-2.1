31bef7f17defc4c81d76a74c42ef1b2b3e560e46
mslancer <wolfnw@yandex.ru>
2021-09-28 21:40:48 +0500

    Build without error

--- src/test/java/ru/netology/rest/PostmanTest.java
+++ src/test/java/ru/netology/rest/PostmanTest.java
@@ -22,7 +22,7 @@ public class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some dataw"));
+                .body("data", equalTo("some data"));
     }
 
 }
