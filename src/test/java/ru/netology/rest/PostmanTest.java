e791b15c63ed1491c1002329081850d3e60b3a41
mslancer <wolfnw@yandex.ru>
2021-09-28 21:39:19 +0500

    test break build

--- src/test/java/ru/netology/rest/PostmanTest.java
+++ src/test/java/ru/netology/rest/PostmanTest.java
@@ -22,7 +22,7 @@ public class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("some dataw"));
     }
 
 }
